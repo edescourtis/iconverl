e65e2b60d7e2bc0fdc51b1c5cecbc3dc443dc1a6
Eric des Courtis <eric.des.courtis@benbria.ca>
2013-11-28 11:05:18 -0500

    Improve spec and return einval when unsupported to conform to old library

--- src/iconv.erl
+++ src/iconv.erl
@@ -23,10 +23,12 @@
 
 -export([open/2, conv/2, close/1]).
 
--spec open(string() | binary(), string() | binary()) -> {ok, any()}.
+-spec open(string() | binary(), string() | binary()) -> {ok, any()} | {error, any()}.
 open(ToCode, FromCode) when is_list(ToCode) or is_binary(ToCode),
     is_list(FromCode) or is_binary(FromCode) ->
     case iconverl:open(ToCode, FromCode) of
+        {error, unsupported} ->
+            {error, einval};
         T when is_tuple(T) ->
             T;
         X ->
